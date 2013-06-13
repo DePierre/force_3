d9edbc45d48281aa85b40e6df85ac74489dfe288
jvoisin <julien.voisin@dustri.org>
2013-06-13 19:40:28 +0200

    Fix red

--- src/jeu.pl
+++ src/jeu.pl
@@ -5,7 +5,7 @@
 empty_board([0, 0, 0, 0, -1, 0, 0, 0, 0]).
 
 % Modification du plateau
-setBoard(Value, 0, [_|T], [Value|T])  :- !.
+setBoard(Value, 0, [_|T], [Value|T]).
 setBoard(Value, Index, [X|T], [X|Y]) :-
     Index > 0,
     Index1 is Index - 1,
@@ -13,18 +13,18 @@ setBoard(Value, Index, [X|T], [X|Y]) :-
 
 % Pose un pion
 move(JR, PL, [-1, C, 0], NPL) :-
-    !, deplacement(JR, PL, [-1, C, 0]),
+    deplacement(JR, PL, [-1, C, 0]),
     setBoard(JR, C, PL, NPL).
 
 % Déplace un pion
 move(JR, PL, [CA, CS, 1], NPL) :-
-    !, deplacement(JR, PL, [CA, CS, 1]),
+    deplacement(JR, PL, [CA, CS, 1]),
     setBoard(0, CA, PL, Temp),
     setBoard(JR, CS, Temp, NPL).
 
 % Déplace une case
 move(_, PL, [CA, CS, 2], NPL) :-
-    !, deplacement(_, PL, [CA, CS, 2]),
+    deplacement(_, PL, [CA, CS, 2]),
     setBoard(-1, CS, PL, Temp),
     nth0(CS, PL, Val),
     setBoard(Val, CA, Temp, NPL).
@@ -36,5 +36,5 @@ move(_, PL, [C1, C2, 3], NPL) :-
     move(_, Temp0, [CI, C2, 2], Temp1),
     setBoard(-1, C2, Temp1, NPL).
 
-get_opponent(1, 2) :- !.
+get_opponent(1, 2).
 get_opponent(2, 1).
